81673e09abf572307b3c43e19a8b49d818754c06
Andy Lee <ltsungmin1345@gmail.com>
2024-07-10 20:07:19 +0800

    edit: add suspense method in success page

--- app/check/success/page.tsx
+++ app/check/success/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import StepperLayout from "@/components/layout/PaymentLayout/StepperLayout";
 import SuccessIcon from "@/components/icon/successIcon";
+import CircularLoading from "@/components/ui/loading/CircularLoading";
 
 function Success() {
   const router = useRouter();
@@ -97,4 +98,12 @@ function Success() {
   );
 }
 
-export default Success;
+function WrappedSuccessPage() {
+  return (
+    <Suspense fallback={<CircularLoading />}>
+      <Success />
+    </Suspense>
+  );
+}
+
+export default WrappedSuccessPage;
